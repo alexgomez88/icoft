6d2d38cf7699b4a546c6be0727d45b124e65f4a3
alejandro <alejandrogomez88@gmail.com>
2021-03-08 16:14:48 -0400

    fast deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Icoft`,
+    description: `Centro de Capacitación y Formación Profesional.`,
+    author: `Alejandro Gómez`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -39,9 +39,8 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          © {new Date().getFullYear()}, Hecho por 
+          <a href="https://alexgomez.dev/">AlexGomez.Dev</a>
         </footer>
       </div>
     </>
